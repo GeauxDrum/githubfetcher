8004ab7e25aba8dbb05fa8015de21b85c08b5a0f
Mike Champagne <geauxdrum@gmail.com>
2020-10-20 16:53:34 -0500

    Updates

---
+++ precache-manifest.2c3d41610e4fdcbecd24165340f3dc58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbf3457d54edd6f01c7b1c510864ab1d",
+    "url": "/githubfetcher/index.html"
+  },
+  {
+    "revision": "b26a1326430dbad74db0",
+    "url": "/githubfetcher/static/css/main.016179fd.chunk.css"
+  },
+  {
+    "revision": "b58c7892958025691f42",
+    "url": "/githubfetcher/static/js/2.9aa57977.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/githubfetcher/static/js/2.9aa57977.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b26a1326430dbad74db0",
+    "url": "/githubfetcher/static/js/main.553d266c.chunk.js"
+  },
+  {
+    "revision": "d75d71d998e018d17577",
+    "url": "/githubfetcher/static/js/runtime-main.860c134e.js"
+  }
+]);
